ca1d6ead58115cb41624a73ed8483d177c2c27c8
Yonas Berhe <yonas.berhe007@gmail.com>
2015-12-24 01:31:38 -0800

    put request to create demo data in a diffrent file called collection.js. Also removed errors

--- meteorironrouter.js
+++ meteorironrouter.js
@@ -1,5 +1,3 @@
-Articles = new Meteor.Collection('articles');
-
 Router.map(function() {
   this.route('about');
   this.route('home', {
@@ -9,14 +7,12 @@ Router.map(function() {
   this.route('articles', {
     data: function() {
       // return Articles.find()
-      return {
-        article: Articles.find()
-      };
+      return Articles.find()
     }
   });
 
   this.route('article', {
-    path '/article/:_id',
+    path: '/article/:_id',
       data: function() {
         return Articles.findOne({
           _id: this.params._id
@@ -27,25 +23,3 @@ Router.map(function() {
   });
 
 });
-
-if (Meteor.isServer) {
-
-  Meteor.startup(function() {
-    if (!Articles.findOne()) {
-      var articles = [{
-        title: 'Article 1',
-        body: 'This is article 1 '
-      }, {
-        title: 'Article 2',
-        body: 'This is article 2 '
-      }, {
-        title: 'Article 3',
-        body: 'This is article 3'
-      }];
-      articles.forEach(function(article) {
-        Articles.insert(article);
-      })
-    }
-
-  });
-}
